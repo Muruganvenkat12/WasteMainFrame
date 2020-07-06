4710cc3076b9819c3b13a67b0b4b44525d32a85f
Murugan <muruganwalker619@gmail.com>
2020-07-06 15:43:15 +0530

    Deleted unwanted comments and print statement from views.py

--- WasteMainframe/urls.py
+++ WasteMainframe/urls.py
@@ -22,5 +22,5 @@ from django.contrib import admin
 from WebEntry import views
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
-    url('imageprocessfun', views.imageprocessfun),
+    url('imageprocess', views.imageprocess),
 ]
